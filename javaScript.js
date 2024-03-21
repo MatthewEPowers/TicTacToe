c8560ad4d1940f4de7c8b60e5700140d594f0700
Matthew Powers <matthew.e.powers@gmail.com>
2024-03-21 18:47:28 -0500

    added activeGame boolian to end game once palyer wins and new setBlock function

--- javaScript.js
+++ javaScript.js
@@ -2,50 +2,67 @@
 
 const blocksPressed = [];
 const blocksTracked = [];
+let activeGame = true;
 
 function gameFunction(clicked_id) {
 	
 
 
-	let this_id = clicked_id;
-	let listLength = blocksPressed.length;
+	let block_id = clicked_id;
+	//let listLength = blocksPressed.length;
 	
-	// Check if the block has been pressed before 
-	if(blocksPressed.includes(this_id)){
-		document.getElementById("blockspressed").innerHTML = this_id + "has been pressed";
+	if(activeGame == true){
+		setBlock(block_id);
+		document.getElementById("isGameTrue").innerHTML = activeGame;
 	}
 	else{
-		document.getElementById("blockspressed").innerHTML = this_id + " has not been pressed";
-		//blocksPressed.push(this_id);
-		if(listLength % 2 == 0) {
-			blocksPressed.push(this_id);
-			blocksTracked.push(this_id + "X");	//change this to a new array to tack which block was pressed by who
-		}
-		else{
-			blocksPressed.push(this_id);
-			blocksTracked.push(this_id + "O"); //change this to a new array to tack which block was pressed by who
-		}
-
-		document.getElementById("testList").innerHTML = blocksPressed;
-		document.getElementById("blocksTracked").innerHTML = blocksTracked;
-		changeBlock(this_id);
-		//winner();
+		document.getElementById("isGameTrue").innerHTML = activeGame;
 	}
 
 	if(blocksPressed.length == 9){
 		document.getElementById("playerTurn").innerHTML = "Game Over!";
 		//winner();
 	}
+	winner();
 
 }
 
+
+function setBlock(clicked_id){
+	let block_id = clicked_id;
+	let listLength = blocksPressed.length;
+	
+	// Check if the block has been pressed before 
+	if(blocksPressed.includes(block_id)){
+		document.getElementById("blockspressed").innerHTML = block_id + "has been pressed";
+	}
+	else{
+		document.getElementById("blockspressed").innerHTML = block_id + " has not been pressed";
+		//blocksPressed.push(this_id);
+		if(listLength % 2 == 0) {
+			blocksPressed.push(block_id);
+			blocksTracked.push(block_id + "X");	//change this to a new array to tack which block was pressed by who
+		}
+		else{
+			blocksPressed.push(block_id);
+			blocksTracked.push(block_id + "O"); //change this to a new array to tack which block was pressed by who
+		}
+
+		document.getElementById("testList").innerHTML = blocksPressed;
+		document.getElementById("blocksTracked").innerHTML = blocksTracked;
+		changeBlock(block_id);
+		//winner();
+	}
+}
+
+
 function changeBlock (clicked_id){
 
-	let this_id = clicked_id;
-	var setBlock = document.getElementById(this_id);
+	let block_id = clicked_id;
+	var setBlock = document.getElementById(block_id);
 	let listLength = blocksPressed.length;
 
-	document.getElementById("test").innerHTML = this_id + " was pressed";
+	document.getElementById("test").innerHTML = block_id + " was pressed";
 	document.getElementById("testList").innerHTML = blocksPressed;
 	document.getElementById("testListLength").innerHTML = listLength;
 	
@@ -64,9 +81,21 @@ function changeBlock (clicked_id){
 
 function winner(){
 	
-
+/*
 	if(blocksPressed.at(1) == "Block1" && blocksPressed.at(3) == "Block2" && blocksPressed.at(5) == "Block3"){
 		document.getElementById("playerwinner").innerHTML = "Player X is the Winner!";
+	}
+	*/
+	//document.getElementById("playerwinner").innerHTML = "Player X is the Winner!";
+
+	if(blocksTracked.includes("Block1X") & blocksTracked.includes("Block2X") & blocksTracked.includes("Block3X")){
+		document.getElementById("playerwinner").innerHTML = "Player X is the Winner!";
+		activeGame = false;
+	}
+
+	if(blocksTracked.includes("Block4X") & blocksTracked.includes("Block5X") & blocksTracked.includes("Block6X")){
+		document.getElementById("playerwinner").innerHTML = "Player X is the Winner!";
+		activeGame = false;
 	}
 
 }
